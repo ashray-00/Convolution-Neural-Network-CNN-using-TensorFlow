d5f5acf8194e6fec940aa2b5feb16a914cbcc120
ashray-00 <adhikariashray@gmail.com>
2020-01-12 02:49:47 +0100

    Added .py files

---
+++ compute_cost.py
@@ -0,0 +1,15 @@
+def compute_cost(Z3, Y):
+    """
+    Computes the cost
+    
+    Arguments:
+    Z3 -- output of forward propagation (output of the last LINEAR unit), of shape (number of examples, 6)
+    Y -- "true" labels vector placeholder, same shape as Z3
+    
+    Returns:
+    cost - Tensor of the cost function
+    """
+    
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = Z3, labels = Y))
+    
+    return cost
